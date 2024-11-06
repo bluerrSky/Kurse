08679baa49536b897505f5afcce6c080b4dc8e67
bluerrSky <rocin158ante@gmail.com>
2024-11-06 21:05:51 +0530

    major overhaul

--- Kurse.c
+++ Kurse.c
@@ -2,6 +2,10 @@
 #include <ctype.h>
 #include <signal.h> 
 #include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 int vimMode = 0;
 int vimMotionCh;
 int vimmedScrX=0, vimmedScrY=0;
@@ -12,43 +16,49 @@ struct prevScrSize{
 	int scrWidth;
 } prevScr; 
 int isChBackSpace  = 0;
-void addChInArr(int *currTextSize, char ch, char* text){
-	int scrY, scrX;
-	getyx(stdscr, scrY, scrX);
-	int indexY = 0,i;
-	for(i = 0; i <= *currTextSize; i++){
-		if(indexY == scrY){
-			break;
-		}
-		if(text[i] == '\n'){
-			indexY++;
-		}
-	}
-	int j;
-	for(j = i; j <= i+scrX-1;  j++){
-		if(text[j] == '\n' && j < scrX){
-			for(int k = *currTextSize; k >= j;  k--){
-				text[k] = text[k-1];
-			}
-			text[j] = ' ';
-			(*currTextSize)++;
-		}
-	}
-	for(int k = *currTextSize; k >= j;  k--){
-		if(k > 0)
-		text[k] = text[k-1];
-	}
-	text[j] = ch;
-	(*currTextSize)++;
+int globalCurrIndex = 0;
+int xHelperVar = 1;
 
+char* sliceString(const char* str, int a, int b) {
+    if (a >= b || a < 0 || b > strlen(str)) {
+        return NULL;    }
+    
+    int length = b - a;
+    char* sliced_str = (char*)malloc((length + 1) * sizeof(char));      
+    if (sliced_str == NULL) {
+        return NULL;     }
+    
+    memcpy(sliced_str, str + a, length); 
+    sliced_str[length] = '\0'; 
+
+    return sliced_str;
+}
+
+				
+void addChInArr(int *currTextSize, char ch, char* text){
+	if(globalCurrIndex < *currTextSize){
+		for(int i = *currTextSize; i > globalCurrIndex; i--){
+			text[i] =  text[i-1];
+		}
+	}
+	text[globalCurrIndex] = ch;
+	globalCurrIndex++;
+	(*currTextSize)++;
 }	
 void handlePress(char *text, int* currTextSize, char ch){ 
 	if(ch == KEY_BACKSPACE || ch == 127 || ch == 8){
 		if(*currTextSize > 0){
 			(*currTextSize)--;
+			globalCurrIndex--;
 			isChBackSpace = 1;
+			if(globalCurrIndex  != *currTextSize){
+				for(int i = globalCurrIndex; i < *currTextSize; i++){
+					text[i] = text[i+1];
+				}
+			}
+			text[*currTextSize] = '\0';
+			
 		}
-		text[*currTextSize] = '0';
 	}else if(ch == KEY_RESIZE){
 	}	
 	else{
@@ -82,7 +92,45 @@ void scrollDown(char* text, int* scrollIndex, int* currTextSize, int* i, int* sc
 	*i = *scrollIndex = l+1;	
 	*scrX = *scrY = 0;
 }
-					
+void moveLineUp(char *text){
+	for(int i = globalCurrIndex; i >= 0; i--){
+		if(text[i] == '\n'){
+			if(text[i-1] != '\n' || (text[i-1] == '\n' && text[globalCurrIndex] == '\n')){
+				if((text[i-1] == '\n' && text[globalCurrIndex] == '\n') && ((i-2) > 0 && text[i-2] != '\n')){
+					globalCurrIndex = i-2;
+				}else{
+					globalCurrIndex = i-1;
+				}
+			}else{	
+				globalCurrIndex= i;
+			}
+	
+			break;	
+		}
+	}
+}	
+void moveLineDown(char *text, int currTextSize){
+	int flag = 0;
+	int charInBetweenLines = 0;
+	for(int i = globalCurrIndex; i <= currTextSize; i++){
+		if(text[i] == '\n' || i == currTextSize){
+			if(!flag){
+				flag = 1;
+				continue;
+			}else{
+				if(charInBetweenLines  == 0){
+					globalCurrIndex = i;
+				}else{
+					globalCurrIndex = i-1;	
+				}
+				break;
+			}
+		}
+		if(flag){
+			charInBetweenLines++;
+		}
+	}	
+}
 void moveUpAtEnd(char* text, int currTextSize, int scrWidth, int scrollIndex){
 	int newY = 0, newX=0, iter =scrollIndex;
 	getyx(stdscr, vimmedScrY, vimmedScrX);
@@ -127,7 +175,7 @@ void displayOnScr(char* text, int currTextSize){
 	int scrY, scrX,scrHeight, scrWidth;
 	getyx(stdscr, scrY, scrX);
 	getmaxyx(stdscr, scrHeight, scrWidth);
-	int i; 
+	int i;
 	int goUp = 0;
 	if(prevScr.scrHeight == scrHeight && prevScr.scrWidth == scrWidth){
 		if(!vimMode){
@@ -155,20 +203,29 @@ void displayOnScr(char* text, int currTextSize){
 			i = currTextSize;
 			switch(vimMotionCh){
 				case 'h':
-					move(scrY, scrX-1);
+					if(globalCurrIndex > 0 && text[globalCurrIndex-1] != '\n'){
+						globalCurrIndex--;
+					}
+					//move(scrY, scrX-1);
 					break;
 				case 'l':
-					move(scrY, scrX+1);
+					if(globalCurrIndex < currTextSize && text[globalCurrIndex+1] != '\n'){
+						globalCurrIndex++;
+					}
+					//move(scrY, scrX+1);
 					break;
 				case 'j':
+					xHelperVar=0;
 					if(scrY == scrHeight-1){	
 						scrollDown(text,&scrollIndex, &currTextSize, &i, &scrY,  &scrX); 
 					}else{
 						moveDownAtEnd(text, currTextSize, scrWidth, scrollIndex);
 
 					}
+					moveLineDown(text, currTextSize);
 					break;	
 				case 'k':	
+					xHelperVar=0;
 					if(scrY == 0){
 						if(scrollIndex != 0){
 							scrX = 0;
@@ -176,7 +233,7 @@ void displayOnScr(char* text, int currTextSize){
 							goUp= 1;
 						}
 					}else{
-						moveUpAtEnd(text, currTextSize, scrWidth, scrollIndex);
+						moveLineUp(text);
 					}
 					break;
 			}
@@ -193,9 +250,15 @@ void displayOnScr(char* text, int currTextSize){
 		clear();
 		scrY = scrX = 0;
 	}
-
-	for(; i < currTextSize; i++){
+	clear();
+	i = scrollIndex;
+	int tempCordX, tempCordY;
+	scrX = 0;scrY = 0;
+	for(; i <= currTextSize; i++){
 		//isprint to handle  the resizing character(unprintable)
+		if(i == globalCurrIndex){
+			tempCordX = scrX;tempCordY = scrY;
+		}
 		if(i >= currTextSize  || i < 0) break;
 		if(isprint(text[i]) || text[i]  == '\n'){
 			if(text[i] == '\n'){
@@ -207,20 +270,32 @@ void displayOnScr(char* text, int currTextSize){
 					scrY++;	
 					scrX=0;
 				}
-				addch(text[i]);		
+				//addch(text[i]);		
 			}
-			move(scrY, scrX);
 		}
-		refresh();
 	}
+	char* slicedStr = sliceString(text, scrollIndex, currTextSize);
+	addstr(slicedStr);
+	FILE* debugFile = fopen("debug.txt","a");
+	fprintf(debugFile, "GlobalCurrIndex: %d\tCurrTextSize: %d\n", globalCurrIndex, currTextSize);
+	fprintf(debugFile,"Printing %s\t",text);
+	fprintf(debugFile,"moving to %d %d\n", tempCordY, tempCordX);
+	fclose(debugFile);
+	move(tempCordY,tempCordX);
+	refresh();
 	if(goUp == 1){
 		move(1,0);
+		moveLineUp(text);
 		moveUpAtEnd(text, currTextSize, scrWidth, scrollIndex);
 		goUp = 0;
 		refresh();
 	}
 }
-	
+
+
+
+
+
 int main(){
 	initscr();
 	//setting scr struct size which works to redraw the screen only if it resized
@@ -243,6 +318,7 @@ int main(){
 		}
 		if(ch == 'i' && vimMode == 1){
 			vimMode = 0;
+			continue;
 		}
 			
 		if(currTextSize == textSize -1){
